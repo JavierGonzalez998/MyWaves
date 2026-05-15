faca01d8f133a18845de970555d015fa2dada037
JavierGonzalez998 <javiergonzaleze98@gmail.com>
2026-05-15 19:33:50 -0400

    hotfixes: scrapper

--- lib/scraper.ts
+++ lib/scraper.ts
@@ -8,6 +8,8 @@ const HEADERS = {
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
   Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
   "Accept-Language": "es-CL,es;q=0.9",
+  Referer: SHOA_URL,
+  Origin: "https://www.shoa.cl",
 };
 
 export async function obtenerUbicaciones(): Promise<Ubicacion[]> {
